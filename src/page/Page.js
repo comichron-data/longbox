59a2cdc1a0bc7680fdc47b8730b5e78046bb568e
Cameron Leslie <cameronleslie@gmail.com>
2017-07-16 07:26:54 -0700

    adding status text and resize

--- src/page/Page.js
+++ src/page/Page.js
@@ -6,13 +6,16 @@ import Spinner from '../spinner/Spinner';
 class Page extends Component {
   render() {
     const style = {
-      "background-image": 'url("http://fillmurray.com/800/1223")',
+      "background-image": `url("${this.props.imageUrl}")`,
       "height": this.props.imageHeight
     };
 
     return (
       <div className="lb-c-page" style={ style }>
-        <Spinner />
+        <Spinner
+          spinnerText="loading"
+          
+        />
       </div>
     );
   }
--- src/spinner/Spinner.js
+++ src/spinner/Spinner.js
@@ -4,7 +4,7 @@ import './Spinner.css';
 class Spinner extends Component {
   render() {
     return (
-      <div className="lb-c-spinner">
+      <div className="lb-c-spinner" data-spinner-text={ this.props.spinnerText }>
         <div className="lb-c-spinner__block lb-c-spinner__block--50 lb-c-spinner__block--animate"></div>
         <div className="lb-c-spinner__block lb-c-spinner__block--50 lb-c-spinner__block--animate"></div>
         <div className="lb-c-spinner__block lb-c-spinner__block--100 lb-c-spinner__block--animate"></div>
