d8ceaf9d4a1e4ec3e68a20d4bbe32a28fee18854
Paul Salaets <psalaets@gmail.com>
2017-07-17 20:44:02 -0400

    pushing this broken mess

--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -7,6 +7,28 @@ class Carousel extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      pages: [
+        {
+          imageUrl: 'http://www.harkavagrant.com/history/wutheringsixsm.png',
+          readyToLoad: true,
+          loaded: false
+        },
+        {
+          imageUrl: 'http://www.harkavagrant.com/history/wutheringsixsm.png',
+          readyToLoad: false,
+          loaded: false
+        },
+        {
+          imageUrl: 'http://www.harkavagrant.com/history/wutheringsixsm.png',
+          readyToLoad: false,
+          loaded: false
+        },
+        {
+          imageUrl: 'http://www.harkavagrant.com/history/wutheringsixsm.png',
+          readyToLoad: false,
+          loaded: false
+        }
+      ],
       slideCount: 4,
       currentSlideIndex: 0
     };
@@ -26,6 +48,23 @@ class Carousel extends Component {
 
   goToPage(pageNumber) {
     if (pageNumber >= 0 && pageNumber < this.state.slideCount) {
+
+      // fake delay just to see it lazy load in
+      setTimeout(() => {
+        const pages = this.state.pages
+          .map((page, index) => {
+            if (index === pageNumber) {
+              return Object.assign({}, page, {readyToLoad: true});
+            } else {
+              return page;
+            }
+          });
+
+        this.setState({
+          pages
+        });
+      }, 1000)
+
       this.setState({
         currentSlideIndex: pageNumber
       });
@@ -38,25 +77,25 @@ class Carousel extends Component {
       transform: `translateX(-${this.state.currentSlideIndex * 100}vw)` // {-(page number index) * 100vw}
     }
 
+    const pages = this.state.pages
+      .map((page, index) => {
+        return (
+          <div key={index} className="lb-c-carousel__item">
+            <Page
+              imageUrl={page.imageUrl}
+              readyToLoad={page.readyToLoad}
+              imageHeight="1481px"
+            />
+          </div>
+        );
+      });
+
     return (
       <div className="lb-c-carousel">
         <button className="lb-c-carousel__nav-backward" onClick={this.handleBackClick}>B</button>
         <button className="lb-c-carousel__nav-forward" onClick={this.handleForwardClick}>F</button>
         <div className="lb-c-carousel__slide" style={styles}>
-          {/* map starts here */}
-          <div className="lb-c-carousel__item">
-            <Page imageUrl="http://www.harkavagrant.com/history/wutheringsixsm.png" imageHeight="1481px" />
-          </div>
-          {/* map ends here */}
-          <div className="lb-c-carousel__item">
-            <Page imageUrl="http://www.harkavagrant.com/history/wutheringsixsm.png" imageHeight="1481px" />
-          </div>
-          <div className="lb-c-carousel__item">
-            <Page imageUrl="http://www.harkavagrant.com/history/wutheringsixsm.png" imageHeight="1481px" />
-          </div>
-          <div className="lb-c-carousel__item">
-            <Page imageUrl="http://www.harkavagrant.com/history/wutheringsixsm.png" imageHeight="1481px" />
-          </div>
+          {pages}
         </div>
       </div>
     );
