08866178c7b4494d9a2b608d1522d4bf28a49a7b
Cameron Leslie <cleslie@aarp.org>
2017-08-23 19:59:56 -0400

    share buttons

--- src/components/fullscreen-button/FullscreenButton.js
+++ src/components/fullscreen-button/FullscreenButton.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './FullscreenButton.css';
 
 class FullscreenButton extends Component {
   constructor(props) {
